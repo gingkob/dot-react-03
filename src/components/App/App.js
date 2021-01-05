a0d76f0a3ef4308fb13a382203cb062f7340e90d
milojica <vladica.deljanin@gmail.com>
2021-01-05 12:31:33 +0100

    more styling and refactoring

---
+++ src/components/App/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Emoji from '../Emoji/Emoji';
+import Instruction from "../Instruction/Instruction";
+import './App.css';
+
+let emojiArr = [
+  {
+    id: 'grinning face',
+    value: "😀"
+  },
+  {
+    id: 'party popper',
+    value: "🎉"
+  },
+  {
+    id: 'woman dancing',
+    value: "💃"
+  }
+]
+
+function App() {
+  const [name, setName] = useState("")
+  const handleEvent = (e) => {
+    setName(e.target.id)
+  }
+  return (
+    <div className="container" style={{ backgroundColor: "green", width: "500px", height: "500px", margin: "80px auto" }}>
+      <h2>Emoji component</h2>
+        <Instruction />
+      <ul>
+        {emojiArr.map(emoji => 
+          <Emoji id={emoji.id} value={emoji.value} onClick={handleEvent} />
+        )}
+      </ul>
+      <div className="emojiNameContainer">
+         <p>{name}</p>
+      </div>     
+    </div>
+  );
+}
+
+export default App;
--- src/components/Instruction/Instruction.js
+++ src/components/Instruction/Instruction.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './Instruction.css'
 import emoji from './emoji.svg'
 
 export default class Instruction extends Component {
   render() {
     return (
       <>
-      <img className="imageE" src={emoji} alt="laughing crying emoji"/>
+      <img className="instruction" src={emoji} alt="laughing crying emoji"/>
       <h6>(click on an emoji to view the emoji short name)</h6>
       </>
     )
